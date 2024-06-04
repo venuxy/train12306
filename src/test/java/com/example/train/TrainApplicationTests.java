f43b38f9004b99bbfd5f496aabd3ad795f6eba20
venux <544536852@qq.com>
2024-06-04 18:35:26 +0800

    初始化

---
+++ src/test/java/com/example/train/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
