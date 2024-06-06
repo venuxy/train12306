5d2e51d0c84d764a7f680a937d61db1f37e333f1
venux <544536852@qq.com>
2024-06-06 17:16:50 +0800

    修改启动日志，增加/member，修改logback日志

--- member/src/main/java/com/venux/train/member/config/MemberApplication.java
+++ member/src/main/java/com/venux/train/member/config/MemberApplication.java
@@ -1,15 +1,23 @@
 package com.venux.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.venux")
 public class MemberApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(MemberApplication.class, args);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
     }
 
 }
