f92c6fbaf82e64721db08320fe36cc404cfca468
venux <544536852@qq.com>
2024-06-19 11:24:15 +0800

    集成校验框架validation

--- member/src/main/java/com/venux/train/member/controller/MemberController.java
+++ member/src/main/java/com/venux/train/member/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.venux.train.common.resp.CommonResp;
 import com.venux.train.member.req.MemberRegisterReq;
 import com.venux.train.member.service.MemberService;
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,7 @@ public class MemberController {
     }
 
     @PostMapping("/register")
-    public CommonResp<Long> register(MemberRegisterReq req){
+    public CommonResp<Long> register(@Valid MemberRegisterReq req){
 
         long register = memberService.register(req);
 
--- member/src/main/java/com/venux/train/member/req/MemberRegisterReq.java
+++ member/src/main/java/com/venux/train/member/req/MemberRegisterReq.java
@@ -1,6 +1,9 @@
 package com.venux.train.member.req;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class MemberRegisterReq {
+    @NotBlank(message = "手机号不能为空")
     private String mobile;
 
     public String getMobile() {
