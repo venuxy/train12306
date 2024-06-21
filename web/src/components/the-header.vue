1e2cb804a0bf1bea3f71ec31563b28f1bd0b7b0e
venux <544536852@qq.com>
2024-06-21 17:45:27 +0800

    vuex（store）配合session缓存，解决浏览器刷新丢失数据的问题

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -3,7 +3,9 @@
     <div class="logo" />
     <div style="float: right; color: white;">
       您好：{{member.mobile}} &nbsp;&nbsp;
-      <router-link to="/login">退出登录</router-link>
+      <router-link to="/login" style="color: white;">
+        退出登录
+      </router-link>
     </div>
       <a-menu
           v-model:selectedKeys="selectedKeys1"
