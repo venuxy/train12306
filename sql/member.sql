071891302f20ecbe585db39f7c3a71be6e7457bd
venux <544536852@qq.com>
2024-06-22 13:47:38 +0800

    增加乘车人表，生成持久层代码

--- sql/member.sql
+++ sql/member.sql
@@ -9,3 +9,16 @@ CREATE TABLE `member` (
 
 
 INSERT INTO `member` (`id`, `mobile`) VALUES (1, '13800000000');
+
+drop table if exists `passenger`;
+create table `passenger` (
+                             `id` bigint not null comment 'id',
+                             `member_id` bigint not null comment '会员id',
+                             `name` varchar(20) not null comment '姓名',
+                             `id_card` varchar(18) not null comment '身份证',
+                             `type` char(1) not null comment '旅客类型|枚举[PassengerTypeEnum]',
+                             `create_time` datetime(3) comment '新增时间',
+                             `update_time` datetime(3) comment '修改时间',
+                             primary key (`id`),
+                             index `member_id_index` (`member_id`)
+) engine=innodb default charset=utf8mb4 comment='乘车人';
