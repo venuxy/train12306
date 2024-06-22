7839cc1f1f7fb44fc1030d410a09763c599ff452
venux <544536852@qq.com>
2024-06-22 11:48:26 +0800

    为axios增加登录相关拦截：请求时带上token，返回时校验返回码是不是401

--- web/src/main.js
+++ web/src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Antd from 'ant-design-vue';
+import Antd, {notification} from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 import * as Icons from '@ant-design/icons-vue';
 import axios from "axios";
@@ -21,11 +21,11 @@ for (const i in icons) {
  */
 axios.interceptors.request.use(function (config) {
     console.log('请求参数：', config);
-    // const _token = store.state.member.token;
-    // if (_token) {
-    //     config.headers.token = _token;
-    //     console.log("请求headers增加token:", _token);
-    // }
+    const _token = store.state.member.token;
+    if (_token) {
+        config.headers.token = _token;
+        console.log("请求headers增加token:", _token);
+    }
     return config;
 }, error => {
     return Promise.reject(error);
@@ -35,15 +35,15 @@ axios.interceptors.response.use(function (response) {
     return response;
 }, error => {
     console.log('返回错误：', error);
-    // const response = error.response;
-    // const status = response.status;
-    // if (status === 401) {
-    //     // 判断状态码是401 跳转到登录页
-    //     console.log("未登录或登录超时，跳到登录页");
-    //     store.commit("setMember", {});
-    //     notification.error({ description: "未登录或登录超时" });
-    //     router.push('/login');
-    // }
+    const response = error.response;
+    const status = response.status;
+    if (status === 401) {
+        // 判断状态码是401 跳转到登录页
+        console.log("未登录或登录超时，跳到登录页");
+        store.commit("setMember", {});
+        notification.error({ description: "未登录或登录超时" });
+        router.push('/login');
+    }
     return Promise.reject(error);
 });
 
--- web/src/views/mainView.vue
+++ web/src/views/mainView.vue
@@ -13,17 +13,30 @@
         <a-layout-content
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
-          Content
+          所有会员总数： {{count}}
         </a-layout-content>
       </a-layout>
     </a-layout>
   </a-layout>
 </template>
-<script setup>
 
+
+
+<script setup>
 import TheHeaderView from "@/components/the-header.vue";
 import TheSiderView from "@/components/the-sider.vue";
-
+import axios from "axios";
+import {notification} from "ant-design-vue";
+import {ref} from "vue";
+const count = ref(0);
+axios.get("/member/member/count").then((response) => {
+  let data = response.data;
+  if (data.success) {
+    count.value = data.content;
+  } else {
+    notification.error({ description: data.message });
+  }
+})
 </script>
 <style>
 #components-layout-demo-top-side-2 .logo {
