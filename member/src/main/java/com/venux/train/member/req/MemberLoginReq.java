f8948e33a7e79c77daa9f06ba270b70e0f4d6bf3
venux <544536852@qq.com>
2024-06-23 10:06:12 +0800

    增加乘车人新增接口，增加字段校验

--- member/src/main/java/com/venux/train/member/req/MemberLoginReq.java
+++ member/src/main/java/com/venux/train/member/req/MemberLoginReq.java
@@ -4,8 +4,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public class MemberLoginReq {
-    @NotBlank(message = "手机号不能为空")
-    @Pattern(regexp = "^1[3-9]\\d{9}$", message = "手机号格式不正确")
+    @NotBlank(message = "【手机号】不能为空")
+    @Pattern(regexp = "^1[3-9]\\d{9}$", message = "【手机号】格式不正确")
     private String mobile;
 
     public String getCode() {
@@ -25,7 +25,7 @@ public class MemberLoginReq {
         return sb.toString();
     }
 
-    @NotBlank(message = "验证码不能为空")
+    @NotBlank(message = "【验证码】不能为空")
     private String code;
     public String getMobile() {
         return mobile;
--- member/src/main/java/com/venux/train/member/req/PassengerSaveReq.java
+++ member/src/main/java/com/venux/train/member/req/PassengerSaveReq.java
@@ -1,16 +1,20 @@
 package com.venux.train.member.req;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.Date;
 
 public class PassengerSaveReq {
     private Long id;
 
+    @NotNull(message = "【会员ID】不能为空")
     private Long memberId;
-
+    @NotBlank(message = "【名字】不能为空")
     private String name;
-
+    @NotBlank(message = "【身份证】不能为空")
     private String idCard;
-
+    @NotBlank(message = "【旅客类型】不能为空")
     private String type;
 
     private Date createTime;
