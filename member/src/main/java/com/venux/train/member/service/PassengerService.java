e0a361c9ebea5c8860957141e4ffb56fc45f38a0
venux <544536852@qq.com>
2024-06-25 11:37:12 +0800

    集成配置helper，实现sql分页功能

--- member/src/main/java/com/venux/train/member/service/PassengerService.java
+++ member/src/main/java/com/venux/train/member/service/PassengerService.java
@@ -3,6 +3,7 @@ package com.venux.train.member.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
+import com.github.pagehelper.PageHelper;
 import com.venux.train.common.context.LoginMemberContext;
 import com.venux.train.common.util.SnowUtil;
 import com.venux.train.member.domain.Passenger;
@@ -36,6 +37,7 @@ public class PassengerService {
         if (ObjectUtil.isNotNull(req.getMemberId())) {
             criteria.andMemberIdEqualTo(req.getMemberId());
         }
+        PageHelper.startPage(2, 2);
         List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
         return BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
     }
