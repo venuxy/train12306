a8ff4bbc37ed7a0bd8b81e3a4821a223f5538747
venux <544536852@qq.com>
2024-06-25 11:24:03 +0800

    增加乘车人列表查询接口，限制只能查看会员当前的乘客列表

--- member/src/main/java/com/venux/train/member/controller/PassengerController.java
+++ member/src/main/java/com/venux/train/member/controller/PassengerController.java
@@ -1,12 +1,17 @@
 package com.venux.train.member.controller;
 
 
+import com.venux.train.common.context.LoginMemberContext;
 import com.venux.train.common.resp.CommonResp;
+import com.venux.train.member.req.PassengerQueryReq;
 import com.venux.train.member.req.PassengerSaveReq;
+import com.venux.train.member.resp.PassengerQueryResp;
 import com.venux.train.member.service.PassengerService;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/passenger")
@@ -16,9 +21,14 @@ public class PassengerController {
 
     @PostMapping("/save")
     public CommonResp<Object> save(@Valid @RequestBody PassengerSaveReq req){
-
         passengerService.save(req);
         return new CommonResp<>();
+    }
+    @GetMapping("/query-list")
+    public CommonResp<List<PassengerQueryResp>> queryList(@Valid PassengerQueryReq req){
+        req.setMemberId(LoginMemberContext.getId());
+        List<PassengerQueryResp> list = passengerService.queryList(req);
+        return new CommonResp<>(list);
     }
 
 }
--- member/src/main/java/com/venux/train/member/service/PassengerService.java
+++ member/src/main/java/com/venux/train/member/service/PassengerService.java
@@ -2,15 +2,19 @@ package com.venux.train.member.service;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
 import com.venux.train.common.context.LoginMemberContext;
 import com.venux.train.common.util.SnowUtil;
 import com.venux.train.member.domain.Passenger;
+import com.venux.train.member.domain.PassengerExample;
 import com.venux.train.member.mapper.PassengerMapper;
+import com.venux.train.member.req.PassengerQueryReq;
 import com.venux.train.member.req.PassengerSaveReq;
+import com.venux.train.member.resp.PassengerQueryResp;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
+import java.util.List;
 
 @Service
 public class PassengerService {
@@ -25,5 +29,14 @@ public class PassengerService {
         passenger.setCreateTime(now);
         passenger.setUpdateTime(now);
         passengerMapper.insert(passenger);
+    }
+    public List<PassengerQueryResp> queryList(PassengerQueryReq req){
+        PassengerExample passengerExample = new PassengerExample();
+        PassengerExample.Criteria criteria = passengerExample.createCriteria();
+        if (ObjectUtil.isNotNull(req.getMemberId())) {
+            criteria.andMemberIdEqualTo(req.getMemberId());
+        }
+        List<Passenger> passengerList = passengerMapper.selectByExample(passengerExample);
+        return BeanUtil.copyToList(passengerList, PassengerQueryResp.class);
     }
 }
