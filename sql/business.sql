c225028ffec1aa06e02d31be568ba1fe9cd12089
venux <544536852@qq.com>
2024-06-29 10:59:35 +0800

    配置business数据库，配置持久层代码生成器

---
+++ sql/business.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `member`;
+
+CREATE TABLE `member` (
+                          `id` BIGINT NOT NULL COMMENT 'id',
+                          `mobile` VARCHAR(11) NOT NULL COMMENT '手机号',
+                          PRIMARY KEY (`id`),
+                          UNIQUE KEY `mobile_unique` (`mobile`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = '会员';
+
+INSERT INTO `member` (`id`, `mobile`) VALUES (1, '13800000000');
+
+
