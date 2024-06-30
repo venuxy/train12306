d72a8773bd1808ab57566d8206423e31116f0033
venux <544536852@qq.com>
2024-06-30 10:57:18 +0800

    集成pinyin-pro，自动生成拼音和首字母

--- admin/src/views/main/station.vue
+++ admin/src/views/main/station.vue
@@ -31,19 +31,20 @@
         <a-input v-model:value="station.name" />
       </a-form-item>
       <a-form-item label="站名拼音">
-        <a-input v-model:value="station.namePinyin" />
+        <a-input v-model:value="station.namePinyin" disabled/>
       </a-form-item>
       <a-form-item label="站名拼音首字母">
-        <a-input v-model:value="station.namePy" />
+        <a-input v-model:value="station.namePy" disabled/>
       </a-form-item>
     </a-form>
   </a-modal>
 </template>
 
 <script>
-import { defineComponent, ref, onMounted } from 'vue';
+import {defineComponent, ref, onMounted, watch} from 'vue';
 import {notification} from "ant-design-vue";
 import axios from "axios";
+import {pinyin} from "pinyin-pro";
 
 export default defineComponent({
   name: "station-view",
@@ -77,7 +78,7 @@ export default defineComponent({
       key: 'namePinyin',
     },
     {
-      title: '站名拼音首字母',
+      title: '拼音首字母',
       dataIndex: 'namePy',
       key: 'namePy',
     },
@@ -86,6 +87,17 @@ export default defineComponent({
       dataIndex: 'operation'
     }
     ];
+
+    // http://pinyin-pro.cn/
+    watch(() => station.value.name, ()=>{
+      if (Tool.isNotEmpty(station.value.name)) {
+        station.value.namePinyin = pinyin(station.value.name, { toneType: 'none'}).replaceAll(" ", "");
+        station.value.namePy = pinyin(station.value.name, { pattern: 'first', toneType: 'none'}).replaceAll(" ", "");
+      } else {
+        station.value.namePinyin = "";
+        station.value.namePy = "";
+      }
+    }, {immediate: true});
 
     const onAdd = () => {
       station.value = {};
