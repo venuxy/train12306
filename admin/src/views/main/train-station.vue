eac58c48af220d0efcd4396729bcd9e38870ab0c
venux <544536852@qq.com>
2024-06-30 12:13:00 +0800

    增加车次下拉选择功能，前端调用后端接口成功

--- admin/src/views/main/train-station.vue
+++ admin/src/views/main/train-station.vue
@@ -218,11 +218,23 @@ export default defineComponent({
       });
     };
 
+    const queryTrainCode = () => {
+      axios.get("/business/admin/train/query-all").then((response) => {
+        let data = response.data;
+        if (data.success) {
+          console.log(data.content);
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    };
+
     onMounted(() => {
       handleQuery({
         page: 1,
         size: pagination.value.pageSize
       });
+      queryTrainCode();
     });
 
     return {
