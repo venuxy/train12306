38eea457bfcc89fff40690fb5b46f70fba72aaa8
venux <544536852@qq.com>
2024-07-01 18:24:38 +0800

    增加batch定时调度模块

---
+++ batch/src/main/java/com/venux/train/batch/controller/TestController.java
@@ -0,0 +1,11 @@
+package com.venux.train.batch.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/hello")
+    public String hello(){return "Hello World Batch!!";}
+}
