38c3e67d663ccbf790afdaeb217a86a8a6e7198d
venux <544536852@qq.com>
2024-07-02 10:27:30 +0800

    springboot自带定时任务优缺点分析

---
+++ batch/src/main/java/com/venux/train/batch/mapper/MemberMapper.java
@@ -0,0 +1,30 @@
+package com.venux.train.batch.mapper;
+
+import com.venux.train.batch.domain.Member;
+import com.venux.train.batch.domain.MemberExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface MemberMapper {
+    long countByExample(MemberExample example);
+
+    int deleteByExample(MemberExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Member record);
+
+    int insertSelective(Member record);
+
+    List<Member> selectByExample(MemberExample example);
+
+    Member selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByExample(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByPrimaryKeySelective(Member record);
+
+    int updateByPrimaryKey(Member record);
+}
