8b006e8d02e9bb4a44d6b1490091c97f56e4b55e
venux <544536852@qq.com>
2024-07-28 23:26:43 +0800

    计算某个票卖出去之后，影响了哪些余票库存，更新到数据库里去

---
+++ business/src/main/java/com/venux/train/business/service/AfterConfirmOrderService.java
@@ -0,0 +1,105 @@
+package com.venux.train.business.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.EnumUtil;
+import cn.hutool.core.util.NumberUtil;
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
+import com.alibaba.fastjson.JSON;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.venux.train.business.domain.*;
+import com.venux.train.business.enums.ConfirmOrderStatusEnum;
+import com.venux.train.business.enums.SeatColEnum;
+import com.venux.train.business.enums.SeatTypeEnum;
+import com.venux.train.business.mapper.ConfirmOrderMapper;
+import com.venux.train.business.mapper.DailyTrainSeatMapper;
+import com.venux.train.business.mapper.cust.DailyTrainTicketMapperCust;
+import com.venux.train.business.req.ConfirmOrderDoReq;
+import com.venux.train.business.req.ConfirmOrderQueryReq;
+import com.venux.train.business.req.ConfirmOrderTicketReq;
+import com.venux.train.business.resp.ConfirmOrderQueryResp;
+import com.venux.train.common.context.LoginMemberContext;
+import com.venux.train.common.exception.BusinessException;
+import com.venux.train.common.exception.BusinessExceptionEnum;
+import com.venux.train.common.resp.PageResp;
+import com.venux.train.common.util.SnowUtil;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class AfterConfirmOrderService {
+    private static final Logger LOG = LoggerFactory.getLogger(AfterConfirmOrderService.class);
+    @Resource
+    private DailyTrainTicketMapperCust dailyTrainTicketMapperCust;
+    @Resource
+    private DailyTrainSeatMapper dailyTrainSeatMapper;
+
+    public void afterDoConfirm(DailyTrainTicket dailyTrainTicket, List<DailyTrainSeat> finalSeatList) {
+        for (DailyTrainSeat dailyTrainSeat : finalSeatList) {
+            DailyTrainSeat seatForUpdate = new DailyTrainSeat();
+            seatForUpdate.setId(dailyTrainSeat.getId());
+            seatForUpdate.setSell(dailyTrainSeat.getSell());
+            seatForUpdate.setUpdateTime(new Date());
+            dailyTrainSeatMapper.updateByPrimaryKeySelective(seatForUpdate);
+
+            // 计算这个站卖出去后，影响了哪些站的余票库存
+            // 参照2-3节 如何保证不超卖、不少卖，还要能承受极高的并发 10:30左右
+            // 影响的库存：本次选座之前没卖过票的，和本次购买的区间有交集的区间
+            // 假设10个站，本次买4~7站
+            // 原售：001000001
+            // 购买：000011100
+            // 新售：001011101
+            // 影响：XXX11111X
+            // Integer startIndex = 4;
+            // Integer endIndex = 7;
+            // Integer minStartIndex = startIndex - 往前碰到的最后一个0;
+            // Integer maxStartIndex = endIndex - 1;
+            // Integer minEndIndex = startIndex + 1;
+            // Integer maxEndIndex = endIndex + 往后碰到的最后一个0;
+            Integer startIndex = dailyTrainTicket.getStartIndex();
+            Integer endIndex = dailyTrainTicket.getEndIndex();
+            char[] chars = seatForUpdate.getSell().toCharArray();
+            Integer maxStartIndex = endIndex - 1;
+            Integer minEndIndex = startIndex + 1;
+            Integer minStartIndex = 0;
+            for (int i = startIndex - 1; i >= 0; i--) {
+                char aChar = chars[i];
+                if (aChar == '1') {
+                    minStartIndex = i + 1;
+                    break;
+                }
+            }
+            LOG.info("影响出发站区间：" + minStartIndex + "-" + maxStartIndex);
+
+            Integer maxEndIndex = seatForUpdate.getSell().length();
+            for (int i = endIndex; i < seatForUpdate.getSell().length(); i++) {
+                char aChar = chars[i];
+                if (aChar == '1') {
+                    maxEndIndex = i;
+                    break;
+                }
+            }
+            LOG.info("影响到达站区间：" + minEndIndex + "-" + maxEndIndex);
+
+            dailyTrainTicketMapperCust.updateCountBySell(
+                    dailyTrainSeat.getDate(),
+                    dailyTrainSeat.getTrainCode(),
+                    dailyTrainSeat.getSeatType(),
+                    minStartIndex,
+                    maxStartIndex,
+                    minEndIndex,
+                    maxEndIndex);
+        }
+    }
+
+
+}
--- business/src/main/java/com/venux/train/business/service/ConfirmOrderService.java
+++ business/src/main/java/com/venux/train/business/service/ConfirmOrderService.java
@@ -44,6 +44,8 @@ public class ConfirmOrderService {
     private DailyTrainCarriageService dailyTrainCarriageService;
     @Resource
     private DailyTrainSeatService dailyTrainSeatService;
+    @Resource
+    private AfterConfirmOrderService afterConfirmOrderService;
 
     public void save(ConfirmOrderDoReq req) {
         DateTime now = DateTime.now();
@@ -178,6 +180,18 @@ public class ConfirmOrderService {
         }
 
         LOG.info("最终选座：{}", finalSeatList);
+
+        // 选中座位后事务处理：
+        // 座位表修改售卖情况sell；
+        // 余票详情表修改余票；
+        // 为会员增加购票记录
+        // 更新确认订单为成功
+        try {
+            afterConfirmOrderService.afterDoConfirm(dailyTrainTicket, finalSeatList);
+        } catch (Exception e) {
+            LOG.error("保存购票信息失败", e);
+            throw new BusinessException(BusinessExceptionEnum.CONFIRM_ORDER_EXCEPTION);
+        }
 
     }
 
