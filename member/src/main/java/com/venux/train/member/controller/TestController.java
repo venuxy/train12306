8ca7d8e5699b170f00779726ed36f67c787f8362
venux <544536852@qq.com>
2024-07-29 17:47:09 +0800

    集成Nacos配置中心

--- member/src/main/java/com/venux/train/member/controller/TestController.java
+++ member/src/main/java/com/venux/train/member/controller/TestController.java
@@ -2,10 +2,12 @@ package com.venux.train.member.controller;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class TestController {
     @Value("${test.nacos}")
     private String testNacos;
