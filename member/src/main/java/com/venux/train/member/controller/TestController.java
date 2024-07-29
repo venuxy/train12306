96989f775017495561c8732b66b0c49f700249bb
venux <544536852@qq.com>
2024-07-29 22:00:20 +0800

    Gateway+Nacos实现按应用名做路由转发

--- member/src/main/java/com/venux/train/member/controller/TestController.java
+++ member/src/main/java/com/venux/train/member/controller/TestController.java
@@ -1,8 +1,10 @@
 package com.venux.train.member.controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,12 @@ public class TestController {
     @Value("${test.nacos}")
     private String testNacos;
 
+    @Autowired
+    Environment environment;
+
     @GetMapping("/hello")
-    public String hello(){return String.format("hello %s", testNacos);}
+    public String hello(){
+        String port = environment.getProperty("local.server.port");
+        return String.format("hello %s  端口 %s", testNacos, port);
+    }
 }
