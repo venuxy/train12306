fbad7fe2f2aa8859d853921674329bb7b5df0c34
venux <544536852@qq.com>
2024-07-29 17:22:18 +0800

    读取配置文件的配置

--- member/src/main/java/com/venux/train/member/controller/TestController.java
+++ member/src/main/java/com/venux/train/member/controller/TestController.java
@@ -1,11 +1,15 @@
 package com.venux.train.member.controller;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.nacos}")
+    private String testNacos;
+
     @GetMapping("/hello")
-    public String hello(){return "Hello World!!";}
+    public String hello(){return String.format("hello %s", testNacos);}
 }
