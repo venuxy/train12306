b60bc05ee0d14b6a0056887fb862a0fb956c5f1f
venux <544536852@qq.com>
2024-07-30 09:38:22 +0800

    去掉mybatis的二级缓存

--- business/src/main/java/com/venux/train/business/domain/Train.java
+++ business/src/main/java/com/venux/train/business/domain/Train.java
@@ -3,7 +3,7 @@ package com.venux.train.business.domain;
 import java.io.Serializable;
 import java.util.Date;
 
-public class Train implements Serializable {
+public class Train {
     private Long id;
 
     private String code;
