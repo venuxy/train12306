0fc72010c90bd6e58e55bd2f291b361f409d6efd
venux <544536852@qq.com>
2024-08-05 11:32:47 +0800

    Sentinel的关联规测试

--- business/src/main/java/com/venux/train/business/controller/TestController.java
+++ business/src/main/java/com/venux/train/business/controller/TestController.java
@@ -1,13 +1,36 @@
 package com.venux.train.business.controller;
 
 
+import cn.hutool.core.util.RandomUtil;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.venux.train.business.service.TestService;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Resource
+    private TestService testService;
+
     @SentinelResource("hello")
     @GetMapping("/hello")
-    public String hello(){return "Hello World Business!!";}
+    public String hello() throws InterruptedException {
+        Thread.sleep(500);
+//        int i = RandomUtil.randomInt(1, 10);
+//        if (i <= 2) {
+//            throw new RuntimeException("测试异常");
+//        }
+        return "Hello World! Business!";
+    }
+
+    @SentinelResource("hello1")
+    @GetMapping("/hello1")
+    public String hello1() throws InterruptedException {
+        Thread.sleep(500);
+        //testService.hello2();
+        return "Hello World! Business1!";
+    }
+
 }
