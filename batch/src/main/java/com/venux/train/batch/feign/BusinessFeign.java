edfb3c54c9a3b6ca262c22e30d6b161bcc13c569
venux <544536852@qq.com>
2024-08-05 16:43:04 +0800

    Sentinel+Feign熔断后进行降级，走备用方案，但是这里的降级配置在Nacos一直无法生效，暂时没有找到原因

--- batch/src/main/java/com/venux/train/batch/feign/BusinessFeign.java
+++ batch/src/main/java/com/venux/train/batch/feign/BusinessFeign.java
@@ -3,6 +3,7 @@ package com.venux.train.batch.feign;
 import com.venux.train.common.resp.CommonResp;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -10,7 +11,8 @@ import java.util.Date;
 
 //@FeignClient(value = "business", fallback = BusinessFeignFallback.class)
 // @FeignClient(name = "business", url = "http://127.0.0.1:8002/business")
-@FeignClient(name = "business")
+@Component
+@FeignClient(value = "business", fallback = BusinessFeignFallback.class)
 public interface BusinessFeign {
 
     @GetMapping("/business/hello")
---
+++ batch/src/main/java/com/venux/train/batch/feign/BusinessFeignFallback.java
@@ -0,0 +1,19 @@
+package com.venux.train.batch.feign;
+
+import com.venux.train.common.resp.CommonResp;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class BusinessFeignFallback implements BusinessFeign {
+    @Override
+    public String hello() {
+        return "Fallback";
+    }
+
+    @Override
+    public CommonResp<Object> genDaily(Date date) {
+        return null;
+    }
+}
