045037d7d331bfbe4b12fd0debb3e9b574f5701c
venux <544536852@qq.com>
2024-08-05 23:02:25 +0800

    使用Redis制作令牌锁防止机器人抢票

--- business/src/main/java/com/venux/train/business/service/SkTokenService.java
+++ business/src/main/java/com/venux/train/business/service/SkTokenService.java
@@ -17,8 +17,12 @@ import com.venux.train.business.req.SkTokenQueryReq;
 import com.venux.train.business.req.SkTokenSaveReq;
 import com.venux.train.business.resp.SkTokenQueryResp;
 import jakarta.annotation.Resource;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -39,6 +43,12 @@ public class SkTokenService {
 
     @Resource
     private SkTokenMapperCust skTokenMapperCust;
+
+    @Autowired
+    private RedissonClient redissonClient;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
     /**
      * 初始化
@@ -114,6 +124,17 @@ public class SkTokenService {
     public boolean validSkToken(Date date, String trainCode, Long memberId) {
          LOG.info("会员【{}】获取日期【{}】车次【{}】的令牌开始", memberId, DateUtil.formatDate(date), trainCode);
 //         令牌约等于库存，令牌没有了，就不再卖票，不需要再进入购票主流程去判断库存，判断令牌肯定比判断库存效率高
+         String lockKey = DateUtil.formatDate(date) + "-" + trainCode + "-" + memberId;
+//         RLock lock = redissonClient.getLock(lockKey);
+//         Boolean setIfAbsent = lock.tryLock(3, 10, TimeUnit.SECONDS);
+         Boolean setIfAbsent = redisTemplate.opsForValue().setIfAbsent(lockKey, lockKey, 5, TimeUnit.SECONDS);
+         if (Boolean.TRUE.equals(setIfAbsent)) {
+             LOG.info("恭喜，抢到令牌锁了！lockKey：{}", lockKey);
+         } else {
+             LOG.info("很遗憾，没抢到令牌锁！lockKey：{}", lockKey);
+             return false;
+         }
+
          int updateCount = skTokenMapperCust.decrease(date, trainCode, 1);
          if (updateCount > 0) {
              return true;
