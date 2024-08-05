718efe6d5dff210a2c2aa51b44f18063ced0a1c4
venux <544536852@qq.com>
2024-08-05 10:11:08 +0800

    Sentinel测试不同限流效果

--- business/src/main/java/com/venux/train/business/controller/TestController.java
+++ business/src/main/java/com/venux/train/business/controller/TestController.java
@@ -1,11 +1,13 @@
 package com.venux.train.business.controller;
 
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @SentinelResource("hello")
     @GetMapping("/hello")
     public String hello(){return "Hello World Business!!";}
 }
